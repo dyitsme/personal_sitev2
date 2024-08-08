0d4f9179fe110d86603e1f6045d4c0d3eaf529eb
Denzel <denzel_bryan_yongco@dlsu.edu.ph>
2024-08-08 21:23:02 +0800

    init

---
+++ components/SkillCard.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps(['title', 'description'])
+</script>
+
+<template>
+  <div class="flex flex-col border">
+    <p class="text-2xl">{{ title }}</p>
+    <p class="text-gray-500">{{ description }}</p>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hi I'm Denzel.
+  </h1>
+  <p>I’m a computer science student who is interested in web development and data science. Outside of work, I like to go to the gym and read books sometimes.</p>
+  Norway has a total area of 385,252 square kilometers and a population of 5,438,657 (December 2020). Norway is bordered by Sweden, Finland and Russia to the north-east, and the Skagerrak to the south, with Denmark on the other side.
+  <h1>Skills</h1>
+  <SkillCard title="web development" description="blogging"/>
+  <SkillCard title="web development" description="blogging"/>
+  <SkillCard title="web development" description="blogging"/>
+</template>
