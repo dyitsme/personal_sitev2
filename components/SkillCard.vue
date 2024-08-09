388c6ed2aeddbc9f9ba1af1b9260bb9740675d8c
Denzel <denzel_bryan_yongco@dlsu.edu.ph>
2024-08-09 16:07:19 +0800

    Added icons and skillcard

--- components/SkillCard.vue
+++ components/SkillCard.vue
@@ -1,10 +1,12 @@
 <script setup>
-defineProps(['title', 'description'])
+defineProps(['icon', 'title', 'description'])
+
 </script>
 
 <template>
-  <div class="flex flex-col border">
-    <p class="text-2xl">{{ title }}</p>
-    <p class="text-gray-500">{{ description }}</p>
+  <div class="flex flex-col bg-gradient-to-r from-white to-gray-100 max-w-[392px] max-h-[246px] px-4 py-4 border border-gray-200 rounded-2xl shadow-sm">
+    <img class="max-w-14 max-h-16" :src=icon alt="icon">
+    <p class="text-2xl pt-2">{{ title }}</p>
+    <p class="text-gray-500 py-8">{{ description }}</p>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,14 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hi I'm Denzel.
-  </h1>
-  <p>I’m a computer science student who is interested in web development and data science. Outside of work, I like to go to the gym and read books sometimes.</p>
-  Norway has a total area of 385,252 square kilometers and a population of 5,438,657 (December 2020). Norway is bordered by Sweden, Finland and Russia to the north-east, and the Skagerrak to the south, with Denmark on the other side.
-  <h1>Skills</h1>
-  <SkillCard title="web development" description="blogging"/>
-  <SkillCard title="web development" description="blogging"/>
-  <SkillCard title="web development" description="blogging"/>
+  <div class="mx-auto max-w-[84%]">
+    <h1 class="text-blue-500 text-6xl font-bold">
+      Hi I'm Denzel.
+    </h1>
+    <p class="text-neutral-400">I'm a computer science student who is interested in web development and data science. Outside of work, I like to go to the gym and read books sometimes.</p>
+    <h1 class="text-5xl">Skills</h1>
+    <div class="flex justify-between">
+      <SkillCard icon="/icons/web.svg" title="Web development" description="I use technologies that suit my needs when building websites"/>
+      <SkillCard icon="/icons/datasci.svg" title="Data science" description="I explore data to make better decisions for myself and for others"/>
+      <SkillCard icon="/icons/uiux.svg" title="UI/UX" description="I do this sometimes to create better websites. I spend some time iterating over designs."/>
+    </div>
+  </div>
 </template>
