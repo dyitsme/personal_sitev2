7de2492bee70ff71fa0c265ac6015ea8bca09670
Denzel <denzel_bryan_yongco@dlsu.edu.ph>
2024-08-14 19:47:34 +0800

    Fixed rendering of projects in projects page

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,9 +1,14 @@
+<script setup>
+defineProps(['title', 'description', 'date', 'cover', 'path'])
+</script>
 <template>
-  <NuxtLink to="#">
+  <NuxtLink :to="path">
     <div class="max-w-[600px] flex flex-col">
-      <img class="max-w-inherit max-h-[328px] rounded-2xl object-cover" src="/img/house.jpg" alt="project">
-      <h1 class="text-xl md:text-3xl py-2">Spotify dataset analysis</h1>
-      <p class="text-gray-400 py-2">Performed EDA and tried different machine learning techniques such as linear regression in order to predict the popularity of a song using different factors.</p>
+      <img class="max-w-inherit max-h-[328px] rounded-2xl object-cover" :src="cover" alt="project">
+      <h1 class="text-xl md:text-3xl py-2">{{ title }}</h1>
+      
+      <p class="text-gray-400">{{ date }}</p>
+      <p class="text-gray-400 py-2">{{ description }}</p>
     </div>
   </NuxtLink>
 </template>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,17 +1,30 @@
+<script setup lang="ts">
+const { data: projects } = await useAsyncData('projects', () => {
+
+  return queryContent('/projects').sort({ date: -1}).find()
+})
+
+</script>
 <template>
   <Navbar/>
   <div class="mx-auto max-w-[84%] flex-col">
     <h1 class="text-2xl md:text-5xl py-12 md:py-16">
       All projects
     </h1>
-    <div class="grid md:grid-col-1 md:grid-cols-2 gap-16 justify-between">
+    <ul class="grid md:grid-col-1 md:grid-cols-2 gap-16 justify-between">
+      <li v-for="project in projects" :key="project._id">
+        <!-- {{ project.title }} -->
+        <ProjectCard :title="project.title" :description="project.description" :date="project.date" :cover="project.cover" :path="project._path"/>
+      </li>
+    </ul>
+    <!-- <div class="grid md:grid-col-1 md:grid-cols-2 gap-16 justify-between">
       <ProjectCard/>
       <ProjectCard/>
       <ProjectCard/>
       <ProjectCard/>
       <ProjectCard/>
       <ProjectCard/>
-    </div>
+    </div> -->
     <PageSpacer/>
   </div>
   <PageFooter/>
